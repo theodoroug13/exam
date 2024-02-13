bbc86f1aa88e3d93ed1d3884a93c03958ceba0ee
theodoroug13 <theodoroug@theodoroug>
2024-02-13 10:04:11 +0200

    created m

---
+++ missing.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int *a, int *b){
+    int tmp=*a;
+    *a=*b;
+    *b=*a;
+}
+
+void insertionsort(int *array, int n){
+    int i,j;
+    for(i=1;i<n;i++){
+        j=i-1;
+        while(j>=0 && array[j]>array[j+1]){
+            swap(&array[j+1],&array[j]);
+            j--;
+        }
+    }
+
+}
+
+int missing(int *array, int n){
+
+}
+
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int *array=malloc(n*sizeof(int));
+    for(int i=0;i<n;i++){
+        scnaf("%d", &array[i]);
+    }
+    printf("missing: %d\n", missing(array,n));
+}
