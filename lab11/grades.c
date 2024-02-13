819d6357eba7ca1f63bca9e6385c57285cec4e2f
theodoroug13 <theodoroug@theodoroug>
2024-02-13 14:58:15 +0200

    commit

--- lab11/grades.c
+++ lab11/grades.c
@@ -3,11 +3,19 @@
 
 int main(){
     FILE *writefile=NULL;
+    FILE *readfile=NULL;
+    
     writefile=fopen("grades.dat","wb");
     char c;
     int i=0;
     while(scanf("%c", &c)!=EOF){
         fwrite(&c,sizeof(char),1,writefile);
     }
+    fclose(writefile);
+    readfile=fopen("grades.dat","rb");
+    while(fread(&c,sizeof(char),1,readfile)){
+        printf("%c", c);
+    }
+    fclose(readfile);
 
 }
